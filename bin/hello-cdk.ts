2e55e16c8ab67908f239f7fe30adbc8d50eaabda
A1nz2802 <a1nzdev28@gmail.com>
2025-03-12 18:48:01 -0500

    feat: add ec2 role example

--- bin/hello-cdk.ts
+++ bin/hello-cdk.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { Ex5Stack } from 'lib/ex5-stack';
+import { Ex6Stack } from 'lib/ex6-stack';
 
 const app = new cdk.App();
 
@@ -8,7 +8,7 @@ if (!process.env.CDK_DEFAULT_ACCOUNT) {
   throw new Error('AWS account ID is missing, please set CDK_DEFAULT_ACCOUNT.')
 }
 
-new Ex5Stack(app, 'Exercise4CdkStack', {
+new Ex6Stack(app, 'Exercise4CdkStack', {
   stackName: 'ex-4',
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
---
+++ lib/ex6-stack.ts
@@ -0,0 +1,106 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { CfnInstance, CfnInternetGateway, CfnRoute, CfnRouteTable, CfnSecurityGroup, CfnSubnet, CfnSubnetRouteTableAssociation, CfnVPC, CfnVPCGatewayAttachment } from 'aws-cdk-lib/aws-ec2';
+import { CfnInstanceProfile, ManagedPolicy, Role, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
+import { Construct } from 'constructs';
+
+
+export class Ex6Stack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    /* new CfnRole(this, '', {
+      roleName: '',
+      assumeRolePolicyDocument: '',
+      managedPolicyArns: ['arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess'],
+      tags: [{key: 'project', value: 'myapp'}, {key: 'Name', value: 'MyVpc'}]
+    }) */
+
+    //* Create VPC
+    const vpc = new CfnVPC(this, 'MyVPC', {
+      cidrBlock: '10.0.0.0/16',
+      enableDnsHostnames: true,
+      enableDnsSupport: true,
+      instanceTenancy: 'default',
+      tags: [{key: 'project', value: 'myapp'}, {key: 'Name', value: 'MyVpc'}]
+    })
+
+    //* Create Subnets
+    const subnet1 = new CfnSubnet(this, 'MySubnet', {
+      vpcId: vpc.attrVpcId,
+      cidrBlock: '10.0.1.0/24',
+      availabilityZone: 'us-east-1a',
+      mapPublicIpOnLaunch: true,
+      tags: [{key: 'project', value: 'myapp'}, {key: 'Name', value: 'MySubnet'}]
+    })
+
+    //* Create Public Route Table
+    const route = new CfnRouteTable(this, 'MyPublic-RT', {
+      vpcId: vpc.attrVpcId,
+      tags: [{key: 'project', value: 'myapp'}, {key: 'Name', value: 'MyPublic-RT'}]
+    })
+
+    //* Attach Route table to subnets
+    new CfnSubnetRouteTableAssociation(this, 'AttachRtToSubnet1', {
+      subnetId: subnet1.attrSubnetId,
+      routeTableId: route.attrRouteTableId,
+    })
+
+    //* Create Internet Gateway
+    const igw = new CfnInternetGateway(this, 'MyIGW', {
+      tags: [{key: 'project', value: 'myapp'}, {key: 'Name', value: 'MyIGW'}]
+    })
+
+    //* Attach IGW to VPC
+    new CfnVPCGatewayAttachment(this, 'AttachVpcToIgw', {
+      vpcId: vpc.attrVpcId,
+      internetGatewayId: igw.attrInternetGatewayId,
+    })
+
+    //* Add new route to route table
+    new CfnRoute(this, 'IgwRoute', {
+      routeTableId: route.attrRouteTableId,
+      destinationCidrBlock: '0.0.0.0/0',
+      gatewayId: igw.attrInternetGatewayId,
+    })
+
+    const securityGroup = new CfnSecurityGroup(this, 'MySG', {
+      vpcId: vpc.attrVpcId,
+      groupName: 'StorageLabs',
+      groupDescription: 'Temporary SG for the Storage Service Labs',
+      securityGroupIngress: [{
+        ipProtocol: 'tcp',
+        toPort: 22,
+        fromPort: 22,
+        cidrIp: '0.0.0.0/0',
+        description: 'SSH Access',
+      }, {
+        ipProtocol: 'tcp',
+        toPort: 80,
+        fromPort: 80,
+        cidrIp: '0.0.0.0/0',
+        description: 'HTTP Access',
+      }],
+      tags: [{key: 'project', value: 'myapp'}, {key: 'Name', value: 'StorageLabs'}]
+    })
+
+    const myRole = new Role(this, 'MyRole', {
+      roleName: 'MyRole',
+      assumedBy: new ServicePrincipal('ec2.amazonaws.com'),
+      managedPolicies: [ManagedPolicy.fromAwsManagedPolicyName('AmazonS3ReadOnlyAccess')]
+    })
+
+    const myProfile = new CfnInstanceProfile(this, 'MyInstanceProfile', {
+      instanceProfileName: 'MyInstanceProfile',
+      roles: [myRole.roleName]
+    })
+    
+    new CfnInstance(this, 'MyEc2', {
+      imageId: 'ami-05b10e08d247fb927',
+      instanceType: 't2.micro',
+      subnetId: subnet1.attrSubnetId,
+      securityGroupIds: [securityGroup.attrId],
+      iamInstanceProfile: myProfile.ref,
+      tags: [{key: 'project', value: 'myapp'}, {key: 'Name', value: 'MyEc2'}]
+    })
+  }
+}
