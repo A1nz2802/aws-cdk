511fb32bceecbe58d8132db8864ef6922307d0c8
A1nz2802 <a1nzdev28@gmail.com>
2025-03-18 02:34:39 -0500

    feat: add simple s3 example

--- bin/hello-cdk.ts
+++ bin/hello-cdk.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { Ex8Stack } from 'lib/ex8-stack';
+import { Ex9Stack } from 'lib/s3/ex9-stack';
 
 const app = new cdk.App();
 
@@ -8,8 +8,8 @@ if (!process.env.CDK_DEFAULT_ACCOUNT) {
   throw new Error('AWS account ID is missing, please set CDK_DEFAULT_ACCOUNT.')
 }
 
-new Ex8Stack(app, 'Exercise7CdkStack', {
-  stackName: 'ex-8',
+new Ex9Stack(app, 'Exercise7CdkStack', {
+  stackName: 'ex-9',
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION || 'us-east-1',
---
+++ lib/s3/ex9-stack.ts
@@ -0,0 +1,35 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { CfnBucket, ObjectOwnership } from 'aws-cdk-lib/aws-s3';
+import { Construct } from 'constructs';
+
+export class Ex9Stack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    new CfnBucket(this, 'MyBucket', {
+      bucketName: 'my-bucket-5wyayv98o5nt70e',
+      ownershipControls: {
+        rules: [{ objectOwnership: ObjectOwnership.BUCKET_OWNER_ENFORCED }]
+      },
+      publicAccessBlockConfiguration: {
+        blockPublicAcls: true,
+        blockPublicPolicy: true,
+        ignorePublicAcls: true,
+        restrictPublicBuckets: true,
+      },
+      versioningConfiguration: {
+        status: 'Suspended',
+      },
+      bucketEncryption: {
+        serverSideEncryptionConfiguration: [{
+          bucketKeyEnabled: true,
+          serverSideEncryptionByDefault: {
+            sseAlgorithm: 'AES256',
+          }
+        }]
+      },
+      objectLockEnabled: false,
+      tags: [{ key: 'project', value: 'myapp' }]
+    })    
+  }
+}
