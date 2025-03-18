4862819a7ad6cf68d092b8b8d113e1ddb580f26c
A1nz2802 <a1nzdev28@gmail.com>
2025-03-18 04:32:20 -0500

    feat: add S3 with IAM user and policies examples

--- bin/hello-cdk.ts
+++ bin/hello-cdk.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { Ex9Stack } from 'lib/s3/ex9-stack';
+import { Ex10Stack } from 'lib/s3/ex10-stack';
 
 const app = new cdk.App();
 
@@ -8,8 +8,8 @@ if (!process.env.CDK_DEFAULT_ACCOUNT) {
   throw new Error('AWS account ID is missing, please set CDK_DEFAULT_ACCOUNT.')
 }
 
-new Ex9Stack(app, 'Exercise7CdkStack', {
-  stackName: 'ex-9',
+new Ex10Stack(app, 'Exercise10CdkStack', {
+  stackName: 'ex-10',
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION || 'us-east-1',
