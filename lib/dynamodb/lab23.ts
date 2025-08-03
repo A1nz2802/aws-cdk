00f95f3b85860719de727c5c4a7ba2847407b25b
A1nz2802 <a1nzdev28@gmail.com>
2025-08-02 21:48:14 -0500

    feat: add dynamodb simple lab

--- lib/dynamodb/lab23.ts
+++ lib/dynamodb/lab23.ts
@@ -1,4 +1,11 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
+import {
+  AttributeType,
+  BillingMode,
+  Table,
+  TableClass,
+  TableEncryption,
+} from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
 
 export class Lab23 extends Stack {
@@ -6,5 +13,48 @@ export class Lab23 extends Stack {
     super(scope, id, props);
 
     this.templateOptions.description = '';
+
+    const forumTable = new Table(this, 'ForumTable', {
+      tableName: 'forum',
+      partitionKey: {
+        name: 'postId',
+        type: AttributeType.STRING,
+      },
+      tableClass: TableClass.STANDARD,
+      billingMode: BillingMode.PROVISIONED,
+      writeCapacity: 1,
+      readCapacity: 1,
+      encryption: TableEncryption.DEFAULT,
+    });
+
+    forumTable
+      .autoScaleReadCapacity({
+        minCapacity: 1,
+        maxCapacity: 10,
+      })
+      .scaleOnUtilization({
+        targetUtilizationPercent: 70,
+      });
+
+    forumTable
+      .autoScaleWriteCapacity({
+        minCapacity: 1,
+        maxCapacity: 10,
+      })
+      .scaleOnUtilization({
+        targetUtilizationPercent: 70,
+      });
+
+    new Table(this, 'StoreTable', {
+      tableName: 'store',
+      partitionKey: {
+        name: 'clientId',
+        type: AttributeType.STRING,
+      },
+      sortKey: {
+        name: 'created',
+        type: AttributeType.STRING,
+      },
+    });
   }
 }
